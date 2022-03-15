888a03810bc2ecff580ad7fccc44870394c42414
Julio Pariona <84955458+JPX-0@users.noreply.github.com>
2022-03-14 22:13:04 -0500

    Update ecommerce.sql
    se elimina la creación de tabla en /utils/ecommerce.sql ya que la tabla será creada en /db/init.js

--- utils/ecommerce.sql
+++ utils/ecommerce.sql
@@ -1,13 +1,4 @@
 CREATE DATABASE IF NOT EXISTS `ecommerce` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `ecommerce`;
 
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(1000) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nameProduct` varchar(50) NOT NULL,
-  `priceProduct` int(1000) NOT NULL,
-  `imageProduct` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 COMMIT;
